9f41c8de1e18d1e4a9f9306ba6c26c035fee6f78
omanvic <omanoyevictor@gmail.com>
2023-05-11 07:34:18 -0700

    updating my code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     argv = sys.argv
     num = len(sys.argv) - 1
     if num == 0:
-        print(f"{0}: argument.")
+        print(f"{0}: arguments")
     elif num >= 1:
         if num == 1:
             arg = 'argument'
