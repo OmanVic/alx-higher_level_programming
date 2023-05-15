6d6318ee83b2f0d5f8680b8d86a8671279487cd4
omanvic <omanoyevictor@gmail.com>
2023-05-15 03:50:54 -0700

    updating task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    i = 0
     for lists in matrix:
+        i = 0
         for item in lists:
             print("{}".format(item), end="")
+            if i < len(lists) - 1:
+                print(" ", end="")
+                i += 1
         print("")
