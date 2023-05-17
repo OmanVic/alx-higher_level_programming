1cb754fad7f65af1c887510d79f01163afae920a
omanvic <omanoyevictor@gmail.com>
2023-05-17 11:00:50 -0700

    code fix advance task

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    squared = [list(map(lambda i: i * i, mat)) for mat in matrix]
-    return squared
+     return [list(map(lambda i: i * i, mat)) for mat in matrix]
