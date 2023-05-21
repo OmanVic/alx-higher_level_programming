1b20108f1d2c4498d4cd2909c144d6533b187efd
omanvic <omanoyevictor@gmail.com>
2023-05-20 17:42:36 -0700

    checking if a linked list has a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - check if a single linked list has a cycle
+ *
+ * @list: pointer to the head of the linkedlist to be checked
+ *
+ * Return: 0 if there is no cycle, otherwise 1
+ *
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr;
+
+	ptr = list;
+	while (ptr)
+	{
+		ptr = ptr->next;
+		if (ptr == NULL)
+			return (0);
+		else if (ptr == list)
+			break;
+	}
+	return (1);
+}
