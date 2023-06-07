9657ddc2552b11fd31714a07cf6764bac03d56d9
omanvic <omanoyevictor@gmail.com>
2023-06-07 05:26:16 -0700

    updating Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@ def safe_print_division(a, b):
     try:
         n = a / b
         print("Inside result: {}".format(n))
-        return n
     except ZeroDivisionError:
         n = None
         print("Inside result: {}".format(n))
+    finally:
         return n
