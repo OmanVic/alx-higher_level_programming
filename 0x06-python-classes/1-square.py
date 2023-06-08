7d3309644ceb5e2cefa5514e45513706cda70617
omanvic <omanoyevictor@gmail.com>
2023-06-07 17:51:42 -0700

    creating a private instance attribute side

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A module that represents a square"""
+
+
+class Square:
+    """A class that set the size of a square"""
+
+    def __init__(self, size):
+        self.__size = size
